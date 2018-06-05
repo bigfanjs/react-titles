9619c2b2780640cb6decbb183e7e360532cf4895
bigfanjs <bigfanjs@gmail.com>
2018-06-05 05:45:58 +0000

    feat: allows users to reference the inner svg element on Title6 component

--- src/Title6.js
+++ src/Title6.js
@@ -97,7 +97,7 @@ class Title extends Component {
     }
 
     render() {
-        const {size, style, x, y} = this.props;
+        const { size, style, innerRef, x, y} = this.props;
         const { texts, scales, gaps, close } = this.state;
         const Motion = (scales[0] && scales[1]) ? RealMotion : FakeMotion;
         const strokeWidth = size * this.strokeWidth / 100;
@@ -113,7 +113,7 @@ class Title extends Component {
 
         return (
             !close &&
-            <svg x={x} y={y} width={size} height={height} style={style}>
+            <svg x={x} y={y} width={size} height={height} style={style} ref={innerRef}>
                 <defs>
                     <clipPath id="clip1">
                         <rect x="0" y="0" width={size} height={middle} />
